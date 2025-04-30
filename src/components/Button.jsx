bbb662cac6418578baaa8b9425062b4a5bbcf43b
KaioAntonio <kaioaar@gmail.com>
2025-04-30 15:08:03 -0300

    feat: add Button variants

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,35 @@
-const Button = ({ children, variant = 'primary', ...rest }) => {
+const Button = ({
+  children,
+  variant = 'primary',
+  size = 'small',
+  className,
+  ...rest
+}) => {
   const getVariantClasses = () => {
     if (variant === 'primary') {
       return 'bg-[#00ADB5] text-white';
+    }
+    if (variant === 'secondary') {
+      return 'bg-[#EEEEEE] text-[#35383E]';
     }
     if (variant === 'ghost') {
       return 'bg-transparent text-[#818181]';
     }
   };
 
+  const getVariantSizeClasses = () => {
+    if (size === 'small') {
+      return 'py-1 text-xs';
+    }
+    if (size === 'large') {
+      return 'py-3 text-sm';
+    }
+  };
+
   return (
     <button
       {...rest}
-      className={`flex items-center gap-2 rounded-lg px-6 py-3 text-xs font-semibold transition hover:opacity-70 ${getVariantClasses()}`}
+      className={`${className} flex items-center justify-center gap-2 rounded-lg px-3 font-semibold transition hover:opacity-70 ${getVariantClasses()} ${getVariantSizeClasses()}`}
     >
       {children}
     </button>
